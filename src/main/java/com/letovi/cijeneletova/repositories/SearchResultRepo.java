6a4689dbc002946dfe8b4fbb993be9ef6bd324e9
kremenovic96 <ranko.kremenovic@gmail.com>
2020-02-22 13:03:14 +0100

    nova repo metoda za get istih objekata

--- src/main/java/com/letovi/cijeneletova/repositories/SearchResultRepo.java
+++ src/main/java/com/letovi/cijeneletova/repositories/SearchResultRepo.java
@@ -1,9 +1,19 @@
 package com.letovi.cijeneletova.repositories;
 
 import com.letovi.cijeneletova.models.SearchResult;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface SearchResultRepo extends CrudRepository<SearchResult, Integer> {
 
-
+    @Query("select s from SearchResult s where s.polazniAerodromIataKod = :polazniAerodromIataKod and s.odredisniAerodromNazivIataKod" +
+            " = :odredisniAerodromNazivIataKod and s.brojPutnika = :brojPutnika and s.datumPolaska = :datumPolaska and s.datumPovratka" +
+            " = :datumPovratka")
+    List<SearchResult> findBySameFields(@Param("polazniAerodromIataKod") String origin, @Param("odredisniAerodromNazivIataKod") String destination,
+                                        @Param("brojPutnika") Integer adults, @Param("datumPolaska")LocalDate departureDate,
+                                        @Param("datumPovratka") LocalDate returnDate);
 }
